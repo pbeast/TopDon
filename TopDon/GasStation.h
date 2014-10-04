3f58a8f3596ccd243b3ec97776d6b9e55fc8d0a0
Pavel Yankelevich <ypavel@gmail.com>
2014-10-04 19:14:33 +0300

    Navigation options and additions to gas station property dialog

---
+++ TopDon/GasStation.h
@@ -0,0 +1,13 @@
+//
+//  GasStation.h
+//  TopDon
+//
+//  Created by Pavel Yankelevich on 10/4/14.
+//  Copyright (c) 2014 Pavel Yankelevich. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface GasStation : MKPointAnnotation
+
+@end
---
+++ TopDon/GasStation.m
@@ -0,0 +1,13 @@
+//
+//  GasStation.m
+//  TopDon
+//
+//  Created by Pavel Yankelevich on 10/4/14.
+//  Copyright (c) 2014 Pavel Yankelevich. All rights reserved.
+//
+
+#import "GasStation.h"
+
+@implementation GasStation
+
+@end
